c4b7037166b65e82806a0996ef1e09ea13ff4351
maksuta <dartmakstlt@gmail.com>
2026-09-05 16:06:15 +0300

    modify main.cpp

--- Example/main.cpp
+++ Example/main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
-
+#include <cmath>
+#include <string>
 int main(){
-    std::cout << "Hello world";
-    std::cout << "\nHow are you?";
+    std::setlocale(LC_ALL, "Russian");
+    std::cout << "Ведите стороны треугольника\n";
+    double a, b, c;
+    std::string input;
+    int cnt = 1;
+    while (cnt < 4) {
+        std::cout << "\nВведитe " << cnt << " сторону\n";
+        std::getline(std::cin, input);
+        try {
+            switch (cnt) {
+            case 1:a = std::stod(input);
+            case 2:b = std::stod(input);
+            case 3:c = std::stod(input);
+            }
+            if (std::stod(input) <= 0) { throw std::logic_error(""); }
+        }
+        catch(...){
+            std::cout << "Неверный ввод,повторите попытку";
+            continue;
+        }
+        cnt++;
+    }
+    if (a + b < c or a + c < b or b + c < a) { std::cout << "Треугольника с такими сторонами е существует";return 0; }
+    double p = (a + b + c) / 2;
+    double S = sqrt(p * (p - a) * (p - b) * (p - c));
+    std::cout << "Площадь: " << S;
     return 0;
 }
